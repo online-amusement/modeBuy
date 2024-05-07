29e5dda12514c2750cb2623296c3e892b0d04b3d
olp <t.miyai0001@gmail.com>
2024-05-08 08:53:14 +0900

    クロスオリジン設定

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['cors']], function(){
+    //Route::get('/login', [App\Http\Controllers\MemeberApiController::class, 'login']);
 });
